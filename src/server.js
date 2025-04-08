f193bbe679b170e3c14b7cab41ca1025ae937c99
unknown <jhashivaniradhekrishna@gmail.com>
2025-04-08 11:11:29 +0530

    Enable CORS for local frontend

--- src/server.js
+++ src/server.js
@@ -10,25 +10,15 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "http://localhost:5173/"
-  );
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE" // Include DELETE here
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
+
+const cors = require("cors");
+
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
+
+
 app.use(express.urlencoded({ extended: true }));
 
 // Swagger Configuration
